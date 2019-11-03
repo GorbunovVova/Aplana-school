5fce57585184b3398a8712613e0002378aa05712
Gorbunov Vova <vovin@list.ru>
2019-11-03 15:52:50 +0300

    task_2

--- aplanaTask2/src/tack2/Task2Main.java
+++ aplanaTask2/src/tack2/Task2Main.java
@@ -2,18 +2,28 @@ package tack2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task2Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    private static Scanner input;
+
+    private static void readFile() {
         System.out.println("Введите путь до файла");
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(System.in);
         File file = new File(input.nextLine().trim());
-        input = new Scanner(file);
+        try {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Путь указан не верно");
+            readFile();
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile();
         //Scanner input = new Scanner(new File("input.txt"));
         Map<String, Integer> words = new TreeMap<>();
+        int maxFrequency = 0;
         while (input.hasNext()) {
             String word = input.next().replaceAll("\\pP", "");
             Integer count = words.get(word);
@@ -21,16 +31,17 @@ public class Task2Main {
                 count = 0;
             }
             words.put(word, ++count);
-        }
-        int maxFrequency = 0;
-        String maxFrequencyWord = "";
-        for (Map.Entry<String, Integer> entry : words.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
-            if (entry.getValue() > maxFrequency) {
-                maxFrequency = entry.getValue();
-                maxFrequencyWord = entry.getKey();
+            if (count > maxFrequency) {
+                maxFrequency = count;
             }
         }
-        System.out.println("Наиболее употребляемое слово - " + maxFrequencyWord + " - " + maxFrequency);
+        List<String> maxFrequencyWords = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : words.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+            if (entry.getValue() == maxFrequency) {
+                maxFrequencyWords.add(entry.getKey());
+            }
+        }
+        System.out.println("Самые часто употребляемые слова: " + maxFrequencyWords + " - " + maxFrequency);
     }
 }
